e352c574894e234e1f459016faf23f0e622f2a56
Dylan Grissom <dgriss295@gmail.com>
2023-06-13 12:16:14 -0500

    made changes based on recommended structure to models

--- models/Thought.js
+++ models/Thought.js
@@ -1,42 +1,61 @@
+// Thought.js
 const mongoose = require('mongoose');
-
 const { Schema } = mongoose;
 
-const ThoughtSchema = new Schema({
-  thoughtText: {
-    type: String,
-    required: true,
-    minlength: 1,
-    maxlength: 280,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-    get: (timestamp) => dateFormat(timestamp),
-  },
-  username: {
-    type: String,
-    required: true,
-  },
-  reactions: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: 'Reaction',
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minlength: 1,
+      maxlength: 280,
     },
-  ],
-},
-{
-  toJSON: {
-    virtuals: true,
-    getters: true,
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (timestamp) => formatDate(timestamp), // Use a getter method to format the timestamp
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [
+      {
+        reactionId: {
+          type: Schema.Types.ObjectId,
+          default: () => new mongoose.Types.ObjectId(),
+        },
+        reactionBody: {
+          type: String,
+          required: true,
+          maxlength: 280,
+        },
+        username: {
+          type: String,
+          required: true,
+        },
+        createdAt: {
+          type: Date,
+          default: Date.now,
+          get: (timestamp) => formatDate(timestamp), // Use a getter method to format the timestamp
+        },
+      },
+    ],
   },
-  id: false,
-});
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
 
-ThoughtSchema.virtual('reactionCount').get(function () {
+// Virtual property to get the count of reactions
+thoughtSchema.virtual('reactionCount').get(function () {
   return this.reactions.length;
 });
 
-const Thought = mongoose.model('Thought', ThoughtSchema);
+const Thought = mongoose.model('Thought', thoughtSchema);
 
 module.exports = Thought;
